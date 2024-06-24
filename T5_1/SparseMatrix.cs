232b88c6ec2e740dcb263fda247e5f83490a2b61
adimitrovska <adimitrovska@tu-sofia.bg>
2024-06-24 19:49:04 +0300

    Fixed Task5_1

--- T5_1/SparseMatrix.cs
+++ T5_1/SparseMatrix.cs
@@ -4,46 +4,47 @@ namespace Homework_5_1
 {
 	public class SparseMatrix : IEnumerable<long>
 	{
-		private Dictionary<(int, int), long> matrix;
-		private int rows;
-		private int columns;
+		private Dictionary<(int, int), long> _matrix;
+		private int _rows;
+		private int _columns;
 
 		public SparseMatrix(int rows, int columns)
 		{
 			if (rows <= 0 || columns <= 0)
 				throw new ArgumentException("Rows and columns must be greater than zero.");
 
-			this.rows = rows;
-			this.columns = columns;
-			matrix = new Dictionary<(int, int), long>();
+			_rows = rows;
+			_columns = columns;
+			_matrix = new Dictionary<(int, int), long>();
 		}
 
 		public long this[int i, int j]
 		{
 			get
 			{
-				if (i < 0 || i >= rows || j < 0 || j >= columns)
+				if (i < 0 || i >= _rows || j < 0 || j >= _columns)
 					throw new IndexOutOfRangeException("Index out of range.");
 
-				return matrix.ContainsKey((i, j)) ? matrix[(i, j)] : 0;
+				_matrix.TryGetValue((i, j), out long value);
+				return value;
 			}
 			set
 			{
-				if (i < 0 || i >= rows || j < 0 || j >= columns)
+				if (i < 0 || i >= _rows || j < 0 || j >= _columns)
 					throw new IndexOutOfRangeException("Index out of range.");
 
 				if (value != 0)
-					matrix[(i, j)] = value;
-				else if (matrix.ContainsKey((i, j)))
-					matrix.Remove((i, j));
+					_matrix[(i, j)] = value;
+				else if (_matrix.ContainsKey((i, j)))
+					_matrix.Remove((i, j));
 			}
 		}
 
 		public IEnumerator<long> GetEnumerator()
 		{
-			for (int i = 0; i < rows; i++)
+			for (int i = 0; i < _rows; i++)
 			{
-				for (int j = 0; j < columns; j++)
+				for (int j = 0; j < _columns; j++)
 				{
 					yield return this[i, j];
 				}
@@ -57,10 +58,10 @@ namespace Homework_5_1
 
 		public IEnumerable<(int, int, long)> GetNonzeroElements()
 		{
-			var sortedKeys = matrix.Keys.OrderBy(key => key.Item2).ThenBy(key => key.Item1);
+			var sortedKeys = _matrix.Keys.OrderBy(key => key.Item2).ThenBy(key => key.Item1);
 			foreach (var key in sortedKeys)
 			{
-				yield return (key.Item1, key.Item2, matrix[key]);
+				yield return (key.Item1, key.Item2, _matrix[key]);
 			}
 		}
 
@@ -69,7 +70,7 @@ namespace Homework_5_1
 			if (x != 0)
 			{
 				int count = 0;
-				foreach (var value in matrix.Values)
+				foreach (var value in _matrix.Values)
 				{
 					if (value == x)
 						count++;
@@ -78,16 +79,16 @@ namespace Homework_5_1
 			}
 			else
 			{
-				return (rows * columns) - matrix.Count;
+				return (_rows * _columns) - _matrix.Count;
 			}
 		}
 
 		public override string ToString()
 		{
 			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < rows; i++)
+			for (int i = 0; i < _rows; i++)
 			{
-				for (int j = 0; j < columns; j++)
+				for (int j = 0; j < _columns; j++)
 				{
 					sb.Append(this[i, j] + "\t");
 				}
